c7d7c3148b859f86662d45cb3bebc0175bceedf5
Akash <akash.akash@soprasteria.com>
2019-07-26 13:38:17 +0530

    fixed issues

--- yelp.py
+++ yelp.py
@@ -163,7 +163,7 @@ def initializeChrome():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
-    #chrome_options.add_argument('--start-maximized')
+    chrome_options.add_argument('--start-maximized')
     #chrome_options.add_argument('user-data-dir=%s/selenium' %(currDir))
     #chrome_options.add_argument('--proxy-server=%s' % PROXY)
     driver = webdriver.Chrome(

3aa80a288f113c696d53fe8ffd446afa99a6f10a
Akash <akash.akash@soprasteria.com>
2019-08-02 11:18:22 +0530

    Doing single country

--- keywords.py
+++ keywords.py
@@ -1,8 +1,8 @@
 keywords=[
           'car accessories stores in %s,%s',
+          'car stereo stores in %s,%s',
+          'auto parts stores in %s,%s',
           'window tinting services in %s,%s',
           'auto glass shops in %s,%s',
-          'car stereo stores in %s,%s',
           'car window film shops in %s,%s',
-          'auto parts stores in %s,%s'
          ]

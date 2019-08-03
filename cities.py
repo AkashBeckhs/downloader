63c3f1b54ee7c8852520c6c470abee229b14074e
Akash <akash.insignia@gmail.com>
2019-08-03 19:33:51 +0530

    belize

--- cities.py
+++ cities.py
@@ -41,4 +41,4 @@ cityDict['antigua and barbuda']=['All Saints','Bolans','Carlisle, Saint George',
 cityDict['Saint Kitts & Nevis']=['Basseterre','Cayon','Charlestown','Dieppe Bay Town','Gingerland','Newcastle','Old Road Town','Sandy Point Town']
 
 
-toDoList=['belize','saint lucia','antigua and barbuda','Saint Kitts & Nevis']
+toDoList=['belize']

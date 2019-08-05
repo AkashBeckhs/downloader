908db10a6427a5808ceee1964145d493aeb02b75
Akash <akash.akash@soprasteria.com>
2019-08-05 16:12:36 +0530

    india

--- keywords.py
+++ keywords.py
@@ -1,8 +1,11 @@
 keywords=[
-          'car accessories stores in %s,%s',
+		  'window tinting services in %s,%s',
+		  'auto glass shops in %s,%s',
+		  'car window film shops in %s,%s',
           'car stereo stores in %s,%s',
-          'auto parts stores in %s,%s',
-          'window tinting services in %s,%s',
-          'auto glass shops in %s,%s',
-          'car window film shops in %s,%s',
+		  #when data is less then following are used
+		  'car accessories stores in %s,%s',
+          'car lightning stores in %s,%s',
+		  'car led stores in %s,%s',
+		  'car xenon stores in %s,%s'
          ]

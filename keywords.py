05b2b97c33383a60fcecca265467a2e575710f3f
Akash <akash.akash@soprasteria.com>
2019-08-07 16:39:47 +0530

    running only last three keywords for india

--- keywords.py
+++ keywords.py
@@ -1,9 +1,9 @@
 keywords=[
-		  'window tinting services in %s,%s',
-		  'auto glass shops in %s,%s',
-		  'car window film shops in %s,%s',
-          'car stereo stores in %s,%s',
-		  'car accessories stores in %s,%s',
+		  #'window tinting services in %s,%s',
+		  #'auto glass shops in %s,%s',
+		  #'car window film shops in %s,%s',
+          #'car stereo stores in %s,%s',
+		  #car accessories stores in %s,%s',
           'car lightning supplier in %s,%s',
 		  'car led supplier in %s,%s',
 		  'car xenon supplier in %s,%s'

0f9c7984e34e5612fa36dabca4f65c0115adc087
Akash <akash.akash@soprasteria.com>
2019-08-12 10:31:34 +0530

    kazakhastan

--- keywords.py
+++ keywords.py
@@ -1,8 +1,8 @@
 keywords=[
-		  #'window tinting services in %s,%s',
-		  #'auto glass shops in %s,%s',
-		  #'car window film shops in %s,%s',
-          #'car stereo stores in %s,%s',
+		  'window tinting services in %s,%s',
+		  'auto glass shops in %s,%s',
+		  'car window film shops in %s,%s',
+          'car stereo stores in %s,%s',
 		  #car accessories stores in %s,%s',
           'car lightning supplier in %s,%s',
 		  'car led supplier in %s,%s',

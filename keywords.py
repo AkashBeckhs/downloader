d2408bae6097e3d0342e7838314e312e0549315a
Akash <akash.akash@soprasteria.com>
2019-08-13 09:58:12 +0530

    keywords changed

--- keywords.py
+++ keywords.py
@@ -1,10 +1,18 @@
+keywords2 = [
+    'window tinting services in %s,%s',
+    'auto glass shops in %s,%s',
+    'car window film shops in %s,%s',
+    'car stereo stores in %s,%s',
+    'car lightning supplier in %s,%s',
+    'car led supplier in %s,%s',
+    'car xenon supplier in %s,%s'
+]
+
 keywords=[
-		  'window tinting services in %s,%s',
-		  'auto glass shops in %s,%s',
-		  'car window film shops in %s,%s',
-          'car stereo stores in %s,%s',
-		  #car accessories stores in %s,%s',
-          'car lightning supplier in %s,%s',
-		  'car led supplier in %s,%s',
-		  'car xenon supplier in %s,%s'
+		  'car window tints shops %s,%s',
+		  'car window film shops %s,%s',
+		  'car audio stores %s,%s',
+          'auto led light stores %s,%s',
+          'auto hid xenon stores %s,%s',
+		  'xenon led stores %s,%s',
          ]

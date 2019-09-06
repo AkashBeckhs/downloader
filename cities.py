f77144dc2135962cc75a64b7c62bac367445829c
Akash <akash.akash@soprasteria.com>
2019-09-06 11:49:14 +0530

    added south ossetia

--- cities.py
+++ cities.py
@@ -20,4 +20,7 @@ cityDict['ukraine']=['Kiev','Kharkiv','Dnipro','Odesa','Donets’k','L’viv','Z
 
 cityDict['lithuania']=['Vilnius','Kaunas','Klaipeda','Šiauliai','Panevezys','Alytus','Marijampole','Mazeikiai','Jonava','Utena','Kedainiai','Telsiai','Visaginas','Taurage','Ukmerge','Kretinga','Radviliskis','Druskininkai','Rokiškis','Kursenai','Jurbarkas','Vilkaviskis']
 
-toDoList=['lithuania']
+
+cityDict['south ossetia']=['Tbilisi','Batumi','Kutaisi','Rustavi','Gori','Zugdidi','Poti','Khashuri','Samtredia','Senaki','Zestafoni','Marneuli','Telavi','Akhaltsikhe','Kobuleti','Ozurgeti','Kaspi','Chiatura','Tsqaltubo','Sagarejo','Gardabani','Borjomi','Tqibuli','Khoni','Bolnisi','Akhalkalaki','Gurjaani','Mtskheta','Qvareli','Akhmeta','Kareli','Lanchkhuti','Tsalenjikha','Dusheti','Sachkhere','Dedoplistsqaro','Lagodekhi','Ninotsminda','Abasha','Tsnori','Terjola','Martvili','Jvari','Khobi','Vani','Baghdati','Vale','Tetritsqaro','Tsalka','Dmanisi','Oni','Ambrolauri','Sighnaghi','Tsageri']
+
+toDoList=['south ossetia']
